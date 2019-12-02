8ce942de1c349ce4ca71b6d2e4d1d9cb95f06f13
Zhiyu Ning <zhiyuning@ucsb.edu>
2019-12-01 19:16:40 -0800

    zn - Location query service has real location data

--- src/main/java/earthquakes/controllers/LocationsController.java
+++ src/main/java/earthquakes/controllers/LocationsController.java
@@ -1,6 +1,6 @@
 package earthquakes.controllers;
 
-import earthquakes.services.EarthquakeQueryService;
+import earthquakes.services.LocationQueryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
@@ -33,7 +33,7 @@ public class LocationsController {
     public String getLocationsResults(Model model, OAuth2AuthenticationToken oAuth2AuthenticationToken,
           LocSearch locSearch) {
 
-	LoationQueryService l = new LocationQueryService();
+	LocationQueryService l = new LocationQueryService();
 
       model.addAttribute("locSearch", locSearch);
       String json = l.getJSON(locSearch.getLocation());
