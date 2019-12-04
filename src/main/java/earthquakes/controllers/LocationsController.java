86393e03a8a287fbd38ddfb19bb30e39dd0fdb03
Zhiyu Ning <zhiyuning@ucsb.edu>
2019-12-04 02:54:03 -0800

    zn - implement the delete botton

--- src/main/java/earthquakes/controllers/LocationsController.java
+++ src/main/java/earthquakes/controllers/LocationsController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.Map;
 import java.util.HashMap;
