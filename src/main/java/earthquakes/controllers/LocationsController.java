de019c2448a071ae03f320caf75e5015f509982d
Zhiyu Ning <zhiyuning@ucsb.edu>
2019-12-04 02:50:09 -0800

    zn -add a botton for delete locations

--- src/main/java/earthquakes/controllers/LocationsController.java
+++ src/main/java/earthquakes/controllers/LocationsController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 
 import java.util.Map;
 import java.util.HashMap;
@@ -72,4 +73,12 @@ public class LocationsController {
       model.addAttribute("locations", locationRepository.findAll());
       return "locations/index";
     }
+
+    @DeleteMapping("/locations/delete/{id}")
+    public String delete(@PathVariable("id") long id, Model model) {
+	Location location = locationRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid courseoffering Id:" + id));
+	locationRepository.delete(location);
+	model.addAttribute("locations", locationRepository.findAll());
+	return "locations/index";
+}
 }
